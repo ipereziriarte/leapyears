314b3178f0d89c198078ed1794b9ee0e27ac6c5c
Imanol <eltigrequehayenti@gmail.com>
2014-07-28 23:51:29 +0200

    All test passing!

--- src/leapyears/LeapYears.java
+++ src/leapyears/LeapYears.java
@@ -8,7 +8,7 @@ public class LeapYears {
 
     public static boolean isLeapYear(int year) {
 
-        return year % 4 == 0;
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 
     }
 
--- test/leapyears/LeapYearsTest.java
+++ test/leapyears/LeapYearsTest.java
@@ -2,15 +2,22 @@ package leapyears;
 
 import junit.framework.TestCase;
 
-import org.junit.Test;
-
 import static leapyears.LeapYears.isLeapYear;
 
 
 public class LeapYearsTest extends TestCase {
 
     public void testFour() throws Exception {
-        assertTrue(isLeapYear(4));
+        assertTrue("Leap year must be divisible by 4", isLeapYear(4));
+    }
+
+    public void testHundred() throws Exception {
+        assertFalse("Leap year must be divisible by 4 but not by 100", isLeapYear(100));
+    }
+
+    public void testFourHundred() throws Exception {
+        assertTrue("Leap year must be divisible by 4 but not by 100 unless is divisible by 400",
+                isLeapYear(1600));
     }
 
 }
